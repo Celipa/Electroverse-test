d5fe1c9eb8041044eb95c02cc0a2f827d2bbe271
Celipa <Saama.alva@hotmail.com>
2024-08-10 14:38:39 +0200

    try again

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-router-dom/',
-})
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  server: {
+    port: 3000
+  }
+});
